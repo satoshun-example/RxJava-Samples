a4dede78f0b54234285678f2c1d20c165dc4567d
SatoShun <shun.sato1@gmail.com>
2017-01-10 00:02:49 +0000

    first

---
+++ app/src/main/java/com/github/satoshun/example/rxjava/sample/MainActivity.java
@@ -0,0 +1,13 @@
+package com.github.satoshun.example.rxjava.sample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main_act);
+  }
+}
